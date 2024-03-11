0c2695511fb39be05d226285a0173bdb24ef9741
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-03-11 16:42:28 +0530

    Google Auth

---
+++ Frontend/Client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LoginButton from './ComponentsLogin/LoginButton'
+import LogoutButton from './ComponentsLogin/LogoutButton'
+import Profile from './ComponentsLogin/Profile'
+
+function App() {
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton/>
+      <Profile />
+    </>
+  )
+}
+
+export default App
