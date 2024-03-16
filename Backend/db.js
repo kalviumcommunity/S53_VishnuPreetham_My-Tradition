8a23d477ad69825d9d9b3c8945edfa0d9da1fc60
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-03-16 11:54:41 +0530

    Data base Connected to the server

---
+++ Backend/db.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const mongoose=require("mongoose")
+const mongoURI=process.env.Mongouri
+const connectDB= async ()=>{
+    try{
+       
+        await mongoose.connect(mongoURI)
+        console.log("DataBase have been succesfully Connected")
+
+    }catch(error){
+        console.log("error:",error)
+        console.log("DataBase have Disconnected ,Please check the errors.")        
+    }
+}
+
+
+
+module.exports=connectDB
