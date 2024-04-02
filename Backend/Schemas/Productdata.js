8b641dec46c00b09f98a2ee47ea4f72c5fd35682
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-04-02 12:06:20 +0530

    Routes changed

--- Backend/Schemas/Productdata.js
+++ Backend/Schemas/Productdata.js
@@ -1,168 +1,76 @@
-const express = require("express")
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ProductRoute = express.Router()
-const { State,Product,sareeProd,Kurtha,Croptop,WeddingModern,Nikkah,Telugu,Haldi,Bengali,AndhraPradesh, Odissa, Kerala, Karnataka, Maharastra, TamilNadu} = require('../Schemas/Productdata.js');
-/////////Get the TamilNadu Dresses;
-ProductRoute.get('/tamilnaduwedding', async (req, res) => {
-    try {
-        const data = await TamilNadu.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Mahrastra Dresses;
-ProductRoute.get('/maharastra', async (req, res) => {
-    try {
-        const data = await Maharastra.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Karnataka Dresses;
-ProductRoute.get('/karnatakawedding', async (req, res) => {
-    try {
-        const data = await Karnataka.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Kerala Dresses;
-ProductRoute.get('/keralawedding', async (req, res) => {
-    try {
-        const data = await Kerala.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Odissa Dresses;
-ProductRoute.get('/odissawedding', async (req, res) => {
-    try {
-        const data = await Odissa.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the And=dra Wedding Dresses;
-ProductRoute.get('/andhrawedding', async (req, res) => {
-    try {
-        const data = await AndhraPradesh.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Haldi Wears;
-ProductRoute.get('/bengali', async (req, res) => {
-    try {
-        const data = await Bengali.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
+
+const imageSchema = new Schema({
+    img: String,
+    imgDescription: String
 });
 
-/////////Get the Haldi Wears;
-ProductRoute.get('/haldi', async (req, res) => {
-    try {
-        const data = await Haldi.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
+
+const eventSchema = new Schema({
+    event_name: String,
+    state: String,
+    tradition: String,
+    cost: String,
+    stock: String,
+    images: [imageSchema] 
 });
-/////////Get the TeluguWedding Sarees;
-ProductRoute.get('/telugu', async (req, res) => {
-    try {
-        const data = await Telugu.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
+
+const stateSchema = new Schema({
+    name: String,
+    events: [{
+        name: String,
+        traditions: [eventSchema] 
+    }]
 });
-////////Get the Nikkah Dresses;
-ProductRoute.get('/nikkah', async (req, res) => {
-    try {
-        const data = await Nikkah.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
+
+
+
+const productSchema = new Schema({
+  img: String,
+  price: Number,
+  Description: String,
+  Aboutproduct: String,
+  color: String,
+  product_details: {
+    description: String,
+    top_type: String,
+    top_style: String,
+    neck_neckline: String,
+    top_pattern: String,
+    sleeve_detail: String,
+    bottom_type: String,
+    dupatta_stole: String,
+    fabric: String
+  },
+  additional_info: {
+    ideal_occasions: [String],
+    accessories: [String]
+  }
 });
-////////Get the WeddingModern Dresses;
-ProductRoute.get('/weddingmoderndress', async (req, res) => {
-    try {
-        const data = await WeddingModern.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-////////Get the Croptop;
-ProductRoute.get('/croptop', async (req, res) => {
-    try {
-        const data = await Croptop.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Kurtha data;
-ProductRoute.get('/kurtha', async (req, res) => {
-    try {
-        const data = await Kurtha.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-/////////Get the Saree data;
-ProductRoute.get('/saree', async (req, res) => {
-    try {
-        const data = await sareeProd.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-////////Get the dupatta data.
-ProductRoute.get('/dupatta', async (req, res) => {
-    try {
-        const data = await Product.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-ProductRoute.get('/getdata', async (req, res) => {
-    try {
-        const data = await State.find().populate({
-            path: 'events.traditions.images',
-            model: 'Image'
-        });
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-module.exports=ProductRoute
+
+const sareeProd=mongoose.model("sarres",productSchema);
+const Kurtha=mongoose.model("Kurtha",productSchema);
+const Product = mongoose.model('Product', productSchema);
+const Croptop = mongoose.model('Croptop', productSchema);
+const WeddingModern = mongoose.model('WeddingModern', productSchema);
+const Nikkah = mongoose.model('Nikkah', productSchema);
+const Telugu = mongoose.model('TeluguWedding', productSchema);
+const Haldi = mongoose.model('Haldi', productSchema);
+const Bengali = mongoose.model('Bengali', productSchema);
+const AndhraPradesh = mongoose.model('Andhrapradesh', productSchema);
+const Odissa = mongoose.model('Odissa', productSchema);
+const Kerala = mongoose.model('Kerala', productSchema);
+const Karnataka = mongoose.model('Karnatakas', productSchema);
+const Maharastra = mongoose.model('Maharastra', productSchema);
+const TamilNadu = mongoose.model('TamilNadu', productSchema);
+
+
+
+
+const Image = mongoose.model('Image', imageSchema);
+const Event = mongoose.model('Event', eventSchema);
+const State = mongoose.model('State', stateSchema);
+
+module.exports = { Image, Event, State,Product,sareeProd,Kurtha,Croptop,WeddingModern,Nikkah,Telugu,Haldi,Bengali,AndhraPradesh,Odissa,Kerala,Karnataka,Maharastra,TamilNadu};
