a113d885bf36a8e0c90c4dee69491765fcace37e
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-04-09 22:23:28 +0530

    Frontend Update and Responsive Done

--- Frontend/Client/src/Components/Mainpages/Navbar.jsx
+++ Frontend/Client/src/Components/Mainpages/Navbar.jsx
@@ -3,24 +3,40 @@ import "../../App.css"
 import logo from "./vecteezy_traditional-colorful-floral-pattern-logo-design-template_7559007-ai.png"
 import { Link } from 'react-router-dom'
 import { Bag2 } from "iconsax-react"
+import {ProfileCircle} from "iconsax-react"
+import {Heart} from "iconsax-react"
 const Navbar = () => {
     return (
         <div>
+            <div className="mainnav">
             <div className="top_quote">
             <p>MAKE YOUR CULTURE MORE EXPRESSIVE WITH YOUR TRADITIONS</p>
             </div>
             <div className="navbar">
-                <div className="Logo">
-                    <img src={logo} alt="" />
-                    <h3>My-Tradition</h3>
+                <div className="GenderType">
+                    <p>WOMEN</p>
+                    <p>MEN</p>
+                    <p>BRIDAL</p>
+                    <p>FESTIVAL</p>
                 </div>
+                {/* <div className="Logo">
+                    <img src={logo} alt="" />
+                    <p>My-Tradition</p>
+                </div> */}
                 <div className="CartBoxes">
-                    <p>Let's Start</p>
                     <Link to={"user_cart"}>
                     <Bag2 size="32" color="#f5ddb0"/>
                     </Link>
+                    <Link to={""}>
+                    <ProfileCircle size="32" color="#ebc5a1"/>
+                    </Link>
+                    <Link to={""}>
+                    <Heart size="32" color="#ebc5a1"/>
+                    </Link>
                 </div>
             </div>
+            </div>
+            
         </div>
     )
 }
