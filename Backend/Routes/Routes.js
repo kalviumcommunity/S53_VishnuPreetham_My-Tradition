ec517c98d7a21e7b94025cb28dfd120864f5565a
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-05-24 09:24:19 +0530

    Get API used to maintain readability of the code and used method called mongoose.model without importing the models from the another file.

--- Backend/Routes/Routes.js
+++ Backend/Routes/Routes.js
@@ -59,5 +59,15 @@ route.post("/wedding/:model", async (req, res) => {
         console.log(error);
     }
 });
-
+route.get('/getProducts/:model', async (req, res) => {
+    const modelName = req.params.model;
+    try {
+        const Model = mongoose.model(modelName);
+        const Products=await Model.find();
+        res.status(200).send(Products);;
+    } catch (error) {
+        res.status(500).send(error); 
+        console.log(error);
+    }
+});
 module.exports=route;
